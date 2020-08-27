320944abbe448def36769f94909831e9971fc5c6
GGfrozen <joen4ik@gmail.com>
2020-08-28 00:27:21 +0300

    Initial commit

---
+++ Assets/Scripts/TestScene/BallMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    [SerializeField] float jumpForce;
+    [SerializeField] float moveSpeed;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Jump();
+        Move();
+    }
+
+    private void Jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector3.up*jumpForce);
+            //rb.AddTorque(Vector3.up * jumpForce);
+        }
+    }
+    private void Move()
+    {
+        float inputZ = Input.GetAxis("Horizontal");
+        float inputX = Input.GetAxis("Vertical");
+        Vector3 moveVector = new Vector3(inputX, 0, -inputZ);
+        rb.AddForce(moveVector * moveSpeed);
+    }
+}
---
+++ Assets/Scripts/TestScene/Cube.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    MeshRenderer cubeRenderer;
+
+
+
+    private void Start()
+    {
+        cubeRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            MeshRenderer ballRenderer = collision.gameObject.GetComponent<MeshRenderer>();
+            Material ballMaterial = ballRenderer.material;
+            cubeRenderer.material = ballMaterial;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            MeshRenderer ballRenderer = other.gameObject.GetComponent<MeshRenderer>();
+            Material ballMaterial = ballRenderer.material;
+            cubeRenderer.material.color = ballMaterial.color;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            cubeRenderer.material.color = Color.yellow;
+        }
+    }
+}
